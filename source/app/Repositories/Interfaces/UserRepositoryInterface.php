0a10455d7e04923075596f6c7df879e2f3fc47a1
THKDUCDEV07\ducde <n_thanhduc@thk-hd.vn>
2025-03-11 10:12:52 +0700

    Create BaseRepositoryInterface for Repository Interface

--- source/app/Repositories/Interfaces/UserRepositoryInterface.php
+++ source/app/Repositories/Interfaces/UserRepositoryInterface.php
@@ -3,14 +3,8 @@
 namespace App\Repositories\Interfaces;
 
 use App\Models\User;
-
-interface UserRepositoryInterface
+use App\Services\Interfaces\BaseRepositoryInterface;
+interface UserRepositoryInterface extends BaseRepositoryInterface
 {
-    /**
-     * Create new user
-     *
-     * @param array $data
-     * @return User
-     */
-    public function create(array $data): User;
+
 }
